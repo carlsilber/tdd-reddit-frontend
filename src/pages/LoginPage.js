bd3e3ec0fe3cc9f8ab9a249f4db0688f11fc0972
Carl Silber <carl.silber74@gmail.com>
2019-12-11 22:07:27 +0200

    handling login button click. closes #13

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,29 +4,51 @@ import Input from '../components/Input';
 export class LoginPage extends React.Component {
     state = {
         username: '',
-        password: ''
+        password: '',
+        apiError: undefined
       };
     
       onChangeUsername = (event) => {
         const value = event.target.value;
         this.setState({
-          username: value
+          username: value,
+          apiError: undefined
         });
       };
     
       onChangePassword = (event) => {
         const value = event.target.value;
         this.setState({
-          password: value
+          password: value,
+          apiError: undefined
+        });
+      };
+
+      onClickLogin = () => {
+        const body = {
+          username: this.state.username,
+          password: this.state.password
+        };
+        this.props.actions.postLogin(body).catch((error) => {
+          if (error.response) {
+            this.setState({ apiError: error.response.data.message });
+          }
         });
       };
     
       render() {
+        let disableSubmit = false;
+        if (this.state.username === '') {
+          disableSubmit = true;
+        }
+        if (this.state.password === '') {
+          disableSubmit = true;
+        }
+    
         return (
           <div className="container">
             <h1 className="text-center">Login</h1>
             <div className="col-12 mb-3">
-              <Input label="Username" placeholder="Your username" />
               <Input
                 label="Username"
                 placeholder="Your username"
@@ -35,7 +57,6 @@ export class LoginPage extends React.Component {
               />
             </div>
             <div className="col-12 mb-3">
-              <Input label="Password" placeholder="Your password" type="password" />
               <Input
                 label="Password"
                 placeholder="Your password"
@@ -44,12 +65,28 @@ export class LoginPage extends React.Component {
                 onChange={this.onChangePassword}
               />
             </div>
+            {this.state.apiError && (
+            <div className="col-12 mb-3">
+                <div className="alert alert-danger">{this.state.apiError}</div>
+            </div>
+        )}
         <div className="text-center">
-          <button className="btn btn-primary">Login</button>
+        <button
+            className="btn btn-primary"
+            onClick={this.onClickLogin}
+            disabled={disableSubmit}
+          >
+            Login
+          </button>
         </div>
       </div>
     );
   }
 }
-
+LoginPage.defaultProps = {
+    actions: {
+      postLogin: () => new Promise((resolve, reject) => resolve({}))
+    }
+  };
+  
 export default LoginPage;
