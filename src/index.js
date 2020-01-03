7f43106740dd8a455d2b1ac17f6a57d8ed647f8a
carl silber <carl.silber@gmail.com>
2020-01-03 22:36:55 +0200

    topbar links connected to redux. closes #22

--- src/index.js
+++ src/index.js
@@ -8,16 +8,24 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import authReducer from './redux/authReducer';
 
-const store = createStore(authReducer);
-  
-  ReactDOM.render(
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>,
-    document.getElementById('root')
-  );
+const loggedInState = {
+  id: 1,
+  username: 'user1',
+  displayName: 'display1',
+  image: 'profile1.png',
+  password: 'P4ssword',
+  isLoggedIn: true
+};
+const store = createStore(authReducer, loggedInState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
