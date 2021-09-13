acd4c59b50ceec8bac9f2e30784e738f7eb213a9
carlsilber <carl.silber74@gmail.com>
2021-09-13 14:04:22 +0300

    implemented user edit layout. closes #46

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
+import Input from './Input';
 
 const ProfileCard = (props) => {
   const { displayName, username, image } = props.user;
 
+  const showEditButton = props.isEditable && !props.inEditMode;
+
   return (
     <div className="card">
       <div className="card-header text-center">
-      <ProfileImageWithDefault
+        <ProfileImageWithDefault
           alt="profile"
           width="200"
           height="200"
@@ -16,11 +19,35 @@ const ProfileCard = (props) => {
         />
       </div>
       <div className="card-body text-center">
-        <h4>{`${displayName}@${username}`}</h4>
-        {props.isEditable && (
-          <button className="btn btn-outline-success">
+        {!props.inEditMode && <h4>{`${displayName}@${username}`}</h4>}
+        {props.inEditMode && (
+          <div className="mb-2">
+            <Input
+              value={displayName}
+              label={`Change Display Name for ${username}`}
+            />
+          </div>
+        )}
+        {showEditButton && (
+          <button
+            className="btn btn-outline-success"
+            onClick={props.onClickEdit}
+          >
             <i className="fas fa-user-edit" /> Edit
           </button>
+        )}
+        {props.inEditMode && (
+          <div>
+            <button className="btn btn-primary">
+              <i className="fas fa-save" /> Save
+            </button>
+            <button
+              className="btn btn-outline-secondary ml-1"
+              onClick={props.onClickCancel}
+            >
+              <i className="fas fa-window-close" /> Cancel
+            </button>
+          </div>
         )}
       </div>
     </div>
