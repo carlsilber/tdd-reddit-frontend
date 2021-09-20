326d8897abe37ed2981e83f1361d2ed27e3e97ec
carlsilber <carl.silber74@gmail.com>
2021-09-20 15:32:45 +0300

    implemented user actions menu layout. closes #57

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -5,11 +5,46 @@ import { connect } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
 
 class TopBar extends React.Component {
+  state = {
+    dropDownVisible: false
+  };
+
+  componentDidMount() {
+    document.addEventListener('click', this.onClickTracker);
+  }
+
+  onClickTracker = (event) => {
+    if (this.actionArea && !this.actionArea.contains(event.target)) {
+      this.setState({
+        dropDownVisible: false
+      });
+    }
+  };
+
+  onClickDisplayName = () => {
+    this.setState({
+      dropDownVisible: true
+    });
+  };
+
   onClickLogout = () => {
+    this.setState({
+      dropDownVisible: false
+    });
     const action = {
       type: 'logout-success'
     };
     this.props.dispatch(action);
+  };
+
+  onClickMyProfile = () => {
+    this.setState({
+      dropDownVisible: false
+    });
+  };
+
+  assignActionArea = (area) => {
+    this.actionArea = area;
   };
 
   render() {
@@ -28,30 +63,46 @@ class TopBar extends React.Component {
       </ul>
     );
     if (this.props.user.isLoggedIn) {
+      let dropDownClass = 'p-0 shadow dropdown-menu';
+      if (this.state.dropDownVisible) {
+        dropDownClass += ' show';
+      }
       links = (
-        <ul className="nav navbar-nav ml-auto">
-          <li className="nav-item nav-link">
-            <ProfileImageWithDefault
-              className="rounded-circle"
-              width="32"
-              height="32"
-              image={this.props.user.image}
-            />
-            {this.props.user.displayName}
-          </li>
-          <li
-            className="nav-item nav-link"
-            onClick={this.onClickLogout}
-            style={{
-              cursor: 'pointer'
-            }}
-          >
-            Logout
-          </li>
-          <li className="nav-item">
-            <Link to={`/${this.props.user.username}`} className="nav-link">
-              My Profile
-            </Link>
+        <ul className="nav navbar-nav ml-auto" ref={this.assignActionArea}>
+          <li className="nav-item dropdown">
+          <div
+              className="d-flex"
+              style={{ cursor: 'pointer' }}
+              onClick={this.onClickDisplayName}
+            >
+              <ProfileImageWithDefault
+                className="rounded-circle m-auto"
+                width="32"
+                height="32"
+                image={this.props.user.image}
+              />
+              <span className="nav-link dropdown-toggle">
+                {this.props.user.displayName}
+              </span>
+            </div>
+            <div className={dropDownClass} data-testid="drop-down-menu">
+              <Link
+                to={`/${this.props.user.username}`}
+                className="dropdown-item"
+                onClick={this.onClickMyProfile}
+              >
+                <i className="fas fa-user text-info"></i> My Profile
+              </Link>
+              <span
+                className="dropdown-item"
+                onClick={this.onClickLogout}
+                style={{
+                  cursor: 'pointer'
+                }}
+              >
+                <i className="fas fa-sign-out-alt text-danger"></i> Logout
+              </span>
+            </div>
           </li>
         </ul>
       );
