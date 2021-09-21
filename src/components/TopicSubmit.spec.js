0e0c18f5c45d52ac5759bb9159c897880aa4afe8
carlsilber <carl.silber74@gmail.com>
2021-09-21 14:28:14 +0300

    implemented connection between redux and topic submit. closes #63

--- src/components/TopicSubmit.spec.js
+++ src/components/TopicSubmit.spec.js
@@ -1,23 +1,51 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import TopicSubmit from './TopicSubmit';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import authReducer from '../redux/authReducer';
+
+const defaultState = {
+  id: 1,
+  username: 'user1',
+  displayName: 'display1',
+  image: 'profile1.png',
+  password: 'P4ssword',
+  isLoggedIn: true
+};
+
+let store;
+
+const setup = (state = defaultState) => {
+  store = createStore(authReducer, state);
+  return render(
+    <Provider store={store}>
+      <TopicSubmit />
+    </Provider>
+  );
+};
 
 describe('TopicSubmit', () => {
   describe('Layout', () => {
     it('has textarea', () => {
-      const { container } = render(<TopicSubmit />);
+        const { container } = setup();
       const textArea = container.querySelector('textarea');
       expect(textArea).toBeInTheDocument();
     });
     it('has image', () => {
-      const { container } = render(<TopicSubmit />);
+        const { container } = setup();
       const image = container.querySelector('img');
       expect(image).toBeInTheDocument();
     });
     it('has textarea', () => {
-      const { container } = render(<TopicSubmit />);
+        const { container } = setup();
       const textArea = container.querySelector('textarea');
       expect(textArea.rows).toBe(1);
     });
+    it('displays user image', () => {
+        const { container } = setup();
+        const image = container.querySelector('img');
+        expect(image.src).toContain('/images/profile/' + defaultState.image);
+      });
   });
 });
