4759e898eb26fe70c745fb746a23be82fee344fd
carlsilber <carl.silber74@gmail.com>
2021-09-24 11:34:16 +0300

    implemented topics in topic feed. closes #71

--- src/components/TopicFeed.js
+++ src/components/TopicFeed.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react'
 import * as apiCalls from '../api/apiCalls';
+import Spinner from './Spinner';
 
 class TopicFeed extends Component {
+    state = {
+        page: {
+          content: []
+        },
+        isLoadingTopics: false
+      };
+    
 componentDidMount() {
-    apiCalls.loadTopics(this.props.user);
+    this.setState({ isLoadingTopics: true });
+    apiCalls.loadTopics(this.props.user).then((response) => {
+      this.setState({ page: response.data, isLoadingTopics: false });
+    });
 }
     render() {
+        if (this.state.isLoadingTopics) {
+            return <Spinner />;
+          }
+          if (this.state.page.content.length === 0) {
+            return (
+              <div className="card card-header text-center">There are no topics</div>
+            );
+          }
+      
         return (
-            <div className="card card-header text-center">There are no topics</div>
+            <div>
+            {this.state.page.content.map((topic) => {
+              return <span key={topic.id}>{topic.content}</span>;
+            })}
+          </div>
         );
     }
 }
--- src/components/TopicFeed.spec.js
+++ src/components/TopicFeed.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitForDomChange, waitForElement } from '@testing-library/react';
 import TopicFeed from './TopicFeed';
 import * as apiCalls from '../api/apiCalls';
 
@@ -10,6 +10,29 @@ const setup = (props) => {
 const mockEmptyResponse = {
   data: {
     content: []
+  }
+};
+
+const mockSuccessGetTopicsSinglePage = {
+  data: {
+    content: [
+      {
+        id: 10,
+        content: 'This is the latest topic',
+        date: 1561294668539,
+        user: {
+          id: 1,
+          username: 'user1',
+          displayName: 'display1',
+          image: 'profile1.png'
+        }
+      }
+    ],
+    number: 0,
+    first: true,
+    last: true,
+    size: 5,
+    totalPages: 1
   }
 };
 
@@ -33,10 +56,42 @@ describe('TopicFeed', () => {
     });
   });
   describe('Layout', () => {
-    it('displays no topic message when the response has empty page', () => {
+    it('displays no topic message when the response has empty page', async () => {
       apiCalls.loadTopics = jest.fn().mockResolvedValue(mockEmptyResponse);
       const { queryByText } = setup();
-      expect(queryByText('There are no topics')).toBeInTheDocument();
+      const message = await waitForElement(() =>
+        queryByText('There are no topics')
+      );
+      expect(message).toBeInTheDocument();
+    });
+    it('does not display no topic message when the response has page of topic', async () => {
+      apiCalls.loadTopics = jest
+        .fn()
+        .mockResolvedValue(mockSuccessGetTopicsSinglePage);
+      const { queryByText } = setup();
+      await waitForDomChange();
+      expect(queryByText('There are no topics')).not.toBeInTheDocument();
+    });
+    it('displays spinner when loading the topics', async () => {
+      apiCalls.loadTopics = jest.fn().mockImplementation(() => {
+        return new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve(mockSuccessGetTopicsSinglePage);
+          }, 300);
+        });
+      });
+      const { queryByText } = setup();
+      expect(queryByText('Loading...')).toBeInTheDocument();
+    });
+    it('displays topic content', async () => {
+      apiCalls.loadTopics = jest
+        .fn()
+        .mockResolvedValue(mockSuccessGetTopicsSinglePage);
+      const { queryByText } = setup();
+      const topicContent = await waitForElement(() =>
+        queryByText('This is the latest topic')
+      );
+      expect(topicContent).toBeInTheDocument();
     });
   });
 });
